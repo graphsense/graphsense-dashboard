972e4e58216fe6079308e731f5c8dee7ac624220
Bernhard Haslhofer <bernhard.haslhofer@ait.ac.at>
2017-10-18 16:42:19 +0200

    bumped version number

--- dashboard.py
+++ dashboard.py
@@ -20,7 +20,7 @@ storage = Storage('localhost', 9000)
 
 @app.context_processor
 def inject_version():
-    return dict(version='Version 0.3.1dev')
+    return dict(version='Version 0.3.1')
 
 
 # TEMPLATE FILTERS
