2290990f7d71464f710fef3f52b3a40ae2eca932
Matthias <git@myrho.net>
2019-11-05 15:23:40 +0100

    fix typo in stats

--- src/pages/statsHtml.js
+++ src/pages/statsHtml.js
@@ -15,7 +15,7 @@ function statsHtml (body) {
         { lastUpdate: (t.format('L') + ' ' + t.format('LT')).replace(/ /g, '&nbsp;'),
           latestBlock: s.no_blocks - 1,
           noAddresses: numeral(s.no_addresses).format(format),
-          noEntities: numeral(s.no_clusters).format(format),
+          noEntities: numeral(s.no_entities).format(format),
           noTransactions: numeral(s.no_transactions).format(format),
           noLabels: numeral(s.no_labels).format(format),
           currency: currencies[keyspace]
