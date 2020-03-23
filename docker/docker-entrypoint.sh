e4c4eb477450e4364fa0a82749d14e5685bd149c
Matthias <git@myrho.net>
2020-03-23 10:45:59 +0100

    fix docker deployment for titanium web service

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 
 sed -i "s|{{REST_ENDPOINT}}|$REST_ENDPOINT|" /usr/share/nginx/html/main.js 
+sed -i "s|{{TITANIUM_REPORT_GENERATION_URL}}|$TITANIUM_REPORT_GENERATION_URL|" /usr/share/nginx/html/main.js 
 
 exec "$@"
