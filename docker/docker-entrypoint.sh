b66854b1c80cbf3122160d1827a76f4396253d24
Matthias <git@myrho.net>
2020-03-25 10:59:38 +0100

    fix env var injection

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 set -e
 
-sed -i "s|{{REST_ENDPOINT}}|$REST_ENDPOINT|" /usr/share/nginx/html/main.js 
-sed -i "s|{{TITANIUM_REPORT_GENERATION_URL}}|$TITANIUM_REPORT_GENERATION_URL|" /usr/share/nginx/html/main.js 
+sed -i "s|{{REST_ENDPOINT}}|$REST_ENDPOINT|g" /usr/share/nginx/html/main.js 
+sed -i "s|{{TITANIUM_REPORT_GENERATION_URL}}|$TITANIUM_REPORT_GENERATION_URL|g" /usr/share/nginx/html/main.js 
 
 exec "$@"
