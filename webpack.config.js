1f17134ca28c08118fc95dde915356534f1741bd
Rainer Simon <aboutgeo@gmail.com>
2022-02-24 07:58:24 +0100

    Changed splash page footer

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 
 const VERSION = '0.5.1'
-const DEV_REST_ENDPOINT = 'http://localhost:9000'
+const DEV_REST_ENDPOINT = 'https://api.graphsense.info/'
 
 const src = path.join(__dirname, 'src')
 
