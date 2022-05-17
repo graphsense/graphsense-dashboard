330efd35a7e1ac29fa8fbe91ef47cee713888b37
Matthias <git@myrho.net>
2022-05-17 14:46:12 +0200

    calc duration

---
+++ src/Locale/Durations.elm
@@ -0,0 +1,72 @@
+module Locale.Durations exposing (Unit(..), durationToString)
+
+import DateTime
+import List.Extra
+import Time
+import Tuple exposing (second)
+
+
+type alias Config =
+    { unitToString : Int -> Unit -> String
+    , separator : String
+    , precision : Int
+    }
+
+
+type Unit
+    = Seconds
+    | Minutes
+    | Hours
+    | Days
+    | Months
+    | Years
+
+
+durationToString : Config -> Int -> String
+durationToString { unitToString, precision, separator } dur =
+    toFloat (Debug.log "dur" dur)
+        / 1000
+        |> abs
+        |> Debug.log "abs"
+        |> (\d ->
+                [ ( Years, 12 )
+                , ( Months, 30.44 )
+                , ( Days, 24 )
+                , ( Hours, 60 )
+                , ( Minutes, 60 )
+                , ( Seconds, 1 )
+                ]
+                    |> List.foldl
+                        (\( unit, base ) { found, range, rest } ->
+                            { found =
+                                ( unit
+                                , rest
+                                    / range
+                                    |> floor
+                                    |> Debug.log "div"
+                                )
+                                    :: found
+                            , rest =
+                                modBy (round range) (round rest)
+                                    |> Debug.log "rest"
+                                    |> toFloat
+                            , range = range / base
+                            }
+                        )
+                        { range = 31560192
+                        , rest = d
+                        , found = []
+                        }
+                    |> .found
+                    |> Debug.log "found"
+                    |> List.reverse
+                    |> List.Extra.dropWhile (second >> (==) 0)
+                    |> List.take precision
+                    |> List.filter (second >> (/=) 0)
+                    |> List.map
+                        (\( unit, c ) ->
+                            unitToString c unit
+                        )
+                    |> List.intersperse separator
+                    |> String.concat
+           )
---
+++ tests/Locale/TestDurations.elm
@@ -0,0 +1,51 @@
+module Locale.TestDurations exposing (..)
+
+import Expect
+import Locale.Durations as Durations
+import Test exposing (..)
+
+
+data =
+    [ ( 0, 3, "" )
+    , ( 12, 3, "12 Seconds" )
+    , ( 12 * 60, 3, "12 Minutes" )
+    , ( 12 * 60 + 1, 3, "12 Minutes 1 Seconds" )
+    , ( 60 * 60 * 3 + 1, 3, "3 Hours 1 Seconds" )
+    , ( 60 * 60 * 3 + 71, 3, "3 Hours 1 Minutes 11 Seconds" )
+    , ( 60 * 60 * 3 + 191, 3, "3 Hours 3 Minutes 11 Seconds" )
+    , ( 60 * 60 * 3 * 24 + 191, 4, "3 Days 3 Minutes 11 Seconds" )
+    , ( 60 * 60 * 3 * 24 + 191, 3, "3 Days 3 Minutes" )
+    , ( 60 * 60 * 3 * 24 + 3791, 4, "3 Days 1 Hours 3 Minutes 11 Seconds" )
+    , ( 60 * 60 * 3 * 24 + 3791, 3, "3 Days 1 Hours 3 Minutes" )
+    , ( 60 * 60 * 3 * 24 * 30.44 + 24 * 60 * 60 * 9 + 3791, 5, "3 Months 9 Days 1 Hours 3 Minutes 11 Seconds" )
+    , ( 60 * 60 * 3 * 24 * 30.44 + 24 * 60 * 60 * 9 + 3791, 4, "3 Months 9 Days 1 Hours 3 Minutes" )
+    , ( 60 * 60 * 3 * 24 * 30.44 + 24 * 60 * 60 * 9 + 3791, 3, "3 Months 9 Days 1 Hours" )
+    , ( 60 * 60 * 3 * 24 * 30.44 + 24 * 60 * 60 * 9 + 3791, 2, "3 Months 9 Days" )
+    , ( 60 * 60 * 3 * 24 * 30.44 + 24 * 60 * 60 * 9 + 3791, 1, "3 Months" )
+    , ( 60 * 60 * 3 * 24 * 30.44 * 13 + 3791, 6, "3 Years 3 Months 1 Hours 3 Minutes 11 Seconds" )
+    , ( 60 * 60 * 3 * 24 * 30.44 * 13 + 3791, 3, "3 Years 3 Months" )
+    , ( -60 * 60 * 24 * 30.44 * 12, 3, "1 Years" )
+    ]
+
+
+config =
+    { unitToString = \i u -> String.fromInt i ++ " " ++ Debug.toString u
+    , separator = " "
+    , precision = 3
+    }
+
+
+suite : Test
+suite =
+    describe "The Durations module"
+        (data
+            |> List.map
+                (\( d, precision, result ) ->
+                    test ("test: " ++ result) <|
+                        \_ ->
+                            Durations.durationToString
+                                { config | precision = precision }
+                                (round <| d * 1000)
+                                |> Expect.equal result
+                )
+        )
