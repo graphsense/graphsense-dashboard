1ef0405d6de6700101660b2de6feb7f3753dfa28
Matthias <git@myrho.net>
2022-06-09 10:51:05 +0200

    remove debug

--- src/Log.elm
+++ src/Log.elm
@@ -3,19 +3,17 @@ module Log exposing (..)
 
 log : String -> a -> a
 log str a =
-    Debug.log str a
-
-
-
---a
+    --Debug.log str a
+    a
 
 
 truncate : String -> a -> a
 truncate str a =
-    let
-        _ =
-            Debug.toString a
-                |> String.left 100
-                |> Debug.log str
-    in
+    {- let
+           _ =
+               Debug.toString a
+                   |> String.left 100
+                   |> Debug.log str
+       in
+    -}
     a
