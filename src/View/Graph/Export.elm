61ac09b4889530d079736569505d429db480b21a
Matthias <git@myrho.net>
2022-06-14 11:21:08 +0200

    export svg, improve tool ui

---
+++ src/View/Graph/Export.elm
@@ -0,0 +1,34 @@
+module View.Graph.Export exposing (..)
+
+import Config.View as View
+import Css.ContextMenu
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (..)
+import Msg.Graph exposing (Msg(..))
+import View.Dialog as Dialog
+import View.Locale as Locale
+
+
+export : View.Config -> List (Html Msg)
+export vc =
+    [ div
+        [ onMouseLeave (UserClickedExport "")
+        ]
+        [ Dialog.part vc
+            "Export"
+            [ option vc "Graphics (.svg)" (UserClickedExportGraphics Nothing)
+            ]
+        ]
+    ]
+
+
+option : View.Config -> String -> msg -> Html msg
+option vc title msg =
+    div
+        [ Css.ContextMenu.option vc |> css
+        , onClick msg
+        ]
+        [ Locale.string vc.locale title
+            |> text
+        ]
