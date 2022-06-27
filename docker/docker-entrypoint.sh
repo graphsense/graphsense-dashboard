80d3bd7314871bbf977218829244dc9202775837
Matthias <git@myrho.net>
2022-06-27 09:46:31 +0200

    fix ingest of rest url

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 set -e
 
-sed -i "s|http://graphsense.rest.local:9000|$REST_URL|g" /usr/share/nginx/html/assets/index.*.js 
+sed -i "s|http://localhost:9000|$REST_URL|g" /usr/share/nginx/html/assets/index.*.js 
 
 exec "$@"
