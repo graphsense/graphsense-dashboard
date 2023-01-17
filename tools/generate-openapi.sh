cf843eca14f1aa9ec6369a9f04a2d10320648267
Matthias <git@myrho.net>
2023-01-17 10:26:22 +0100

    workaround fix openapi gen

--- tools/generate-openapi.sh
+++ tools/generate-openapi.sh
@@ -17,6 +17,7 @@ fi
 temp2=`mktemp`
 echo "Generating"
 python3 $dir/mangle-openapi.py $temp $resturl > $temp2
+
 docker run --rm \
     -v "${dir}/../openapi:/build"  \
     -v "${temp2}:/spec.yaml" \
@@ -31,3 +32,4 @@ rm $temp $temp2
 
 sed -i 's/"txType"/"tx_type"/' "${dir}/../openapi/src/Api/Data.elm"
 sed -i 's/tx_hash/txHash/' "${dir}/../openapi/src/Api/Request/Txs.elm"
+printf "\n\nvaluesDecodervaluesDecoder = valuesDecoder" >> "${dir}/../openapi/src/Api/Data.elm"
