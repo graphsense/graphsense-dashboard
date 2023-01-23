c51b53af24e9c0bc7cf5edeb569104e34931cf83
Matthias <git@myrho.net>
2023-01-23 20:43:37 +0100

    display tokens on edge labels

--- src/Model/Graph/Link.elm
+++ src/Model/Graph/Link.elm
@@ -1,6 +1,7 @@
 module Model.Graph.Link exposing (..)
 
 import Api.Data
+import Dict exposing (Dict)
 
 
 type alias Link a =
@@ -16,14 +17,16 @@ type LinkData
         { labels : Maybe (List String)
         , noTxs : Int
         , value : Api.Data.Values
+        , tokenValues : Maybe (Dict String Api.Data.Values)
         }
     | PlaceholderLinkData
 
 
-fromNeighbor : { a | labels : Maybe (List String), noTxs : Int, value : Api.Data.Values } -> LinkData
-fromNeighbor { labels, noTxs, value } =
+fromNeighbor : { a | labels : Maybe (List String), noTxs : Int, value : Api.Data.Values, tokenValues : Maybe (Dict String Api.Data.Values) } -> LinkData
+fromNeighbor { labels, noTxs, value, tokenValues } =
     LinkData
         { labels = labels
         , noTxs = noTxs
         , value = value
+        , tokenValues = tokenValues
         }
