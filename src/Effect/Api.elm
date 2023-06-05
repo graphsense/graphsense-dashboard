150775cc53978e5ad05db0817c188c1a560e325b
Matthias <git@myrho.net>
2023-06-05 14:50:19 +0200

    test without authorization header

--- src/Effect/Api.elm
+++ src/Effect/Api.elm
@@ -672,7 +672,7 @@ withAuthorization apiKey request =
 
 send : String -> (Result ( Http.Error, eff ) ( Dict String String, msg ) -> msg) -> eff -> (a -> msg) -> Api.Request a -> Cmd msg
 send apiKey wrapMsg effect toMsg =
-    withAuthorization apiKey
+    withAuthorization ""
         >> Api.sendAndAlsoReceiveHeaders wrapMsg effect toMsg
 
 
