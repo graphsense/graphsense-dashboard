906d51ca30acf63e9563e12d6b5cc37493965faa
Matthias <git@myrho.net>
2023-09-07 12:47:16 +0200

    move contextmenu below cursor

--- src/View/Graph/ContextMenu.elm
+++ src/View/Graph/ContextMenu.elm
@@ -15,8 +15,8 @@ import Msg.Graph exposing (..)
 view : View.Config -> Coords -> List (Html Msg) -> Html Msg
 view vc coords =
     div
-        [ top (px coords.y)
-            :: left (px coords.x)
+        [ top (px (coords.y - 5))
+            :: left (px (coords.x - 5))
             :: zIndex (int 100)
             :: Css.ContextMenu.root vc
             |> css
