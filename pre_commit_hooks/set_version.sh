f2482dff92d82062b2c896d022e5e8d589fd16e6
Matthias <git@myrho.net>
2024-05-08 10:36:42 +0200

    improve set_version hook

--- pre_commit_hooks/set_version.sh
+++ pre_commit_hooks/set_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env /bin/bash
 
 tag=`git describe --tags --abbrev=0`
-echo $tag
+echo "Setting version in src/Version.elm to $tag. Please commit and try again."
 #commit=`git log -n 1 --pretty=format:"%h"`
 
 cat > src/Version.elm <<EOF
