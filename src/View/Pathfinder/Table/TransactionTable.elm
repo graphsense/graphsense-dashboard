3abed7489ff3b241832ccf0ae22635c89f2dda50
Matthias <git@myrho.net>
2024-07-15 08:06:29 +0200

    revert tx table to coded component

--- src/View/Pathfinder/Table/TransactionTable.elm
+++ src/View/Pathfinder/Table/TransactionTable.elm
@@ -2,16 +2,18 @@ module View.Pathfinder.Table.TransactionTable exposing (config, prepareCSV)
 
 import Api.Data
 import Config.View as View
+import Css
 import Css.Table exposing (Styles)
-import Html.Styled
+import Html.Styled.Attributes exposing (css)
 import Init.Pathfinder.Id as Id
 import Model.Currency exposing (asset)
 import Model.Locale
 import Model.Pathfinder.Id as Id exposing (Id)
 import Msg.Pathfinder exposing (Msg(..))
+import RecordSetter exposing (s_checkboxes, s_txListCellTimestamp)
 import Table
 import Theme.Html.SidebarComponents as SidebarComponents
-import Util.View exposing (truncateLongIdentifier)
+import Util.View exposing (none, truncateLongIdentifier)
 import View.Graph.Table
 import View.Locale as Locale
 import View.Pathfinder.Table exposing (customizations)
@@ -52,55 +54,94 @@ config styles vc network isCheckedFn =
                 { isChecked = toGerneric >> getId >> isCheckedFn
                 , onClick = UserClickedTxCheckboxInTable
                 }
-            , View.Graph.Table.htmlColumn
-                styles
-                vc
+            , PT.timestampDateMultiRowColumn vc
                 "Timestamp"
                 (toGerneric >> .timestamp)
-                (\data ->
-                    let
-                        d =
-                            toGerneric data |> .timestamp
-                    in
-                    SidebarComponents.txListCellTimestamp
-                        { checkboxes = {}
-                        , txListCellTimestamp =
-                            { checkbox = False
-                            , date =
-                                Locale.timestampDateUniform vc.locale d
-                            , time =
-                                Locale.timestampTimeUniform vc.locale d
-                            }
-                        }
-                        |> List.singleton
-                )
-            , View.Graph.Table.htmlColumnWithSorter
-                Table.unsortable
-                styles
-                vc
-                "Hash"
-                (toGerneric >> .txHash)
-                (\data ->
-                    let
-                        d =
-                            toGerneric data
-                    in
-                    SidebarComponents.txListCellValue
-                        { txListCellValue =
-                            { txValue = truncateLongIdentifier d.txHash
-                            }
-                        }
-                        |> List.singleton
-                )
-            , PT.valueColumnWithOptions
-                { sortable = False
-                , hideCode = False
-                , hideFlowIndicator = False
+            , PT.txColumn vc
+                { label = "Hash"
+                , accessor = toGerneric >> .txHash
+                , onClick = Just (toGerneric >> getId >> UserClickedTx)
                 }
-                vc
+            , PT.debitCreditColumn vc
                 (toGerneric >> .asset >> asset network)
                 "Debit/Credit"
                 (toGerneric >> .value)
+
+            {-
+               , View.Graph.Table.htmlColumn
+                   styles
+                   vc
+                   "Timestamp"
+                   (toGerneric >> .timestamp)
+                   (\data ->
+                       let
+                           d =
+                               toGerneric data |> .timestamp
+                       in
+                       SidebarComponents.txListCellTimestampWithInstances
+                           (SidebarComponents.txListCellTimestampAttributes
+                               |> s_txListCellTimestamp [ css [ Css.width Css.auto ] ]
+                           )
+                           (SidebarComponents.txListCellTimestampInstances
+                               |> s_checkboxes (Just none)
+                           )
+                           { checkboxes = {}
+                           , txListCellTimestamp =
+                               { checkbox = False
+                               , date =
+                                   Locale.timestampDateUniform vc.locale d
+                               , time =
+                                   Locale.timestampTimeUniform vc.locale d
+                               }
+                           }
+                           |> List.singleton
+                   )
+               , View.Graph.Table.htmlColumnWithSorter
+                   Table.unsortable
+                   styles
+                   vc
+                   "Hash"
+                   (toGerneric >> .txHash)
+                   (\data ->
+                       let
+                           d =
+                               toGerneric data
+                       in
+                       SidebarComponents.txListCellValue
+                           { txListCellValue =
+                               { txValue = truncateLongIdentifier d.txHash
+                               }
+                           }
+                           |> List.singleton
+                   )
+               , View.Graph.Table.htmlColumnWithSorter
+                   Table.unsortable
+                   styles
+                   vc
+                   "Debit/Credit"
+                   (toGerneric >> .asset >> asset network)
+                   (\data ->
+                       let
+                           d =
+                               toGerneric data
+                       in
+                       SidebarComponents.txListCellValue
+                           { txListCellValue =
+                               { txValue = truncateLongIdentifier d.txHash
+                               }
+                           }
+                           |> List.singleton
+                   )
+               , PT.valueColumnWithOptions
+                   { sortable = False
+                   , hideCode = False
+                   , hideFlowIndicator = False
+                   }
+                   vc
+                   (toGerneric >> .asset >> asset network)
+                   "Debit/Credit"
+                   (toGerneric >> .value)
+            -}
             ]
         , customizations = customizations vc
         }
