b7e0f1ccedd8e72ec0eddab17b168d999f0aa5b7
Matthias <git@myrho.net>
2024-08-28 17:06:37 +0200

    make text node replacable

--- codegen/src/Generate/Html/TextNode.elm
+++ codegen/src/Generate/Html/TextNode.elm
@@ -5,6 +5,7 @@ import Elm
 import Elm.Op
 import Gen.Html.Styled
 import Gen.Html.Styled.Attributes as Attributes
+import Gen.Maybe
 import Generate.Common.DefaultShapeTraits as Common
 import Generate.Html.DefaultShapeTraits as DefaultShapeTraits
 import Generate.Html.TypeStyle as TypeStyle
@@ -14,21 +15,24 @@ import Types exposing (Config, Details)
 
 toExpressions : Config -> String -> TextNode -> List Elm.Expression
 toExpressions config componentName node =
-    Gen.Html.Styled.call_.div
-        (Common.getName node
-            |> getElementAttributes config
-            |> Elm.Op.append
-                ([ toStyles node |> Attributes.css ]
+    Elm.get (Common.getName node) config.instances
+        |> Gen.Maybe.withDefault
+            (Gen.Html.Styled.call_.div
+                (Common.getName node
+                    |> getElementAttributes config
+                    |> Elm.Op.append
+                        ([ toStyles node |> Attributes.css ]
+                            |> Elm.list
+                        )
+                )
+                (getTextProperty componentName config.propertyExpressions node.defaultShapeTraits.isLayerTrait.componentPropertyReferences
+                    |> Maybe.map Gen.Html.Styled.call_.text
+                    |> Maybe.withDefault (Gen.Html.Styled.text node.characters)
+                    |> withVisibility componentName config.propertyExpressions node.defaultShapeTraits.isLayerTrait.componentPropertyReferences
+                    |> List.singleton
                     |> Elm.list
                 )
-        )
-        (getTextProperty componentName config.propertyExpressions node.defaultShapeTraits.isLayerTrait.componentPropertyReferences
-            |> Maybe.map Gen.Html.Styled.call_.text
-            |> Maybe.withDefault (Gen.Html.Styled.text node.characters)
-            |> withVisibility componentName config.propertyExpressions node.defaultShapeTraits.isLayerTrait.componentPropertyReferences
-            |> List.singleton
-            |> Elm.list
-        )
+            )
         |> List.singleton
 
 
