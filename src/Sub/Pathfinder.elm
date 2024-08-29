8bcf657e7c32779f802b747fd2aa7205a1b922c4
Matthias <git@myrho.net>
2024-08-29 16:44:52 +0200

    improve delete key handling

--- src/Sub/Pathfinder.elm
+++ src/Sub/Pathfinder.elm
@@ -11,50 +11,73 @@ import Sub.Graph.Transform as Transform
 import Time
 
 
-keyDecoder : (String -> Msg) -> Decode.Decoder Msg
+keyDecoder : (String -> Decode.Decoder Msg) -> Decode.Decoder Msg
 keyDecoder kMap =
-    Decode.map kMap (Decode.field "key" Decode.string)
+    Decode.andThen kMap (Decode.field "key" Decode.string)
 
 
-toKeyDown : String -> Msg
+toKeyDown : String -> Decode.Decoder Msg
 toKeyDown keyValue =
     case keyValue of
         "Control" ->
             -- https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
-            UserPressedCtrlKey
+            Decode.succeed UserPressedCtrlKey
 
         "z" ->
             UserPressedNormalKey keyValue
+                |> Decode.succeed
 
         "y" ->
             UserPressedNormalKey keyValue
+                |> Decode.succeed
 
         _ ->
             NoOp
+                |> Decode.succeed
 
 
-toKeyUp : String -> Msg
+toKeyUp : String -> Decode.Decoder Msg
 toKeyUp keyValue =
+    let
+        decodeDeleteOnInput =
+            Decode.at [ "target", "nodeName" ] Decode.string
+                |> Decode.andThen
+                    (\nodeName ->
+                        if nodeName == "INPUT" then
+                            Decode.fail "on input"
+
+                        else
+                            Decode.succeed UserReleasedDeleteKey
+                    )
+    in
     case keyValue of
         "Control" ->
             -- https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
             UserReleasedCtrlKey
+                |> Decode.succeed
 
         "z" ->
             UserReleasedNormalKey keyValue
+                |> Decode.succeed
 
         "y" ->
             UserReleasedNormalKey keyValue
+                |> Decode.succeed
+
+        "Backspace" ->
+            decodeDeleteOnInput
 
         "Delete" ->
             -- https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
-            UserReleasedDeleteKey
+            decodeDeleteOnInput
 
         "Escape" ->
             UserReleasedEscape
+                |> Decode.succeed
 
         _ ->
             NoOp
+                |> Decode.succeed
 
 
 subscriptions : Model -> Sub Msg
