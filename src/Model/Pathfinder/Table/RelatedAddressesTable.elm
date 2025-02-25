049c6db528a0631e6be4a3df86d0d4b0a7b8c206
Matthias <git@myrho.net>
2025-02-25 15:21:26 +0100

    calm linter

--- src/Model/Pathfinder/Table/RelatedAddressesTable.elm
+++ src/Model/Pathfinder/Table/RelatedAddressesTable.elm
@@ -1,22 +1,5 @@
 module Model.Pathfinder.Table.RelatedAddressesTable exposing (Model)
 
-import Api.Data
-import Api.Request.Addresses
-import Model.DateRangePicker as DateRangePicker
-import Model.Graph.Table as Table
-import Model.Pathfinder.PagedTable exposing (PagedTable)
-import Msg.Pathfinder.AddressDetails exposing (Msg)
-
 
 type alias Model =
     {}
-
-
-titleAddress : String
-titleAddress =
-    "Address"
-
-
-titleValue : String
-titleValue =
-    "Value"
