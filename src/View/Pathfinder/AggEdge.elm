989c16bde114bc669884a77fb261a204105373bc
Matthias <git@myrho.net>
2025-06-25 10:53:45 +0200

    improve aggedge rendering

--- src/View/Pathfinder/AggEdge.elm
+++ src/View/Pathfinder/AggEdge.elm
@@ -10,26 +10,29 @@ import Model.Pathfinder.AggEdge exposing (AggEdge)
 import Msg.Pathfinder exposing (Msg(..))
 import RecordSetter exposing (s_dividerLine, s_leftArrow, s_leftArrowGroup, s_leftValue, s_rectangleOfAggregatedLabel, s_rightArrow, s_rightArrowGroup, s_rightValue, s_root)
 import RemoteData
-import Svg.Styled exposing (Svg, g, line)
+import Svg.PathD exposing (Segment(..), pathD)
+import Svg.Styled exposing (Svg, g, path)
 import Svg.Styled.Attributes as Svg exposing (css, transform, width)
 import Theme.Svg.GraphComponents as GraphComponents
 import Theme.Svg.GraphComponentsAggregatedTracing as Theme
 import Util.Graph exposing (translate)
 import Util.View exposing (onClickWithStop)
 import View.Locale as Locale
-import View.Pathfinder.Tx.Utils exposing (toPosition)
+import View.Pathfinder.Tx.Utils exposing (Pos, signX, toPosition)
 
 
 type alias Dimensions =
     { x : Float
     , y : Float
+    , totalWidth : Float
     , leftLabelWidth : Float
     , rightLabelWidth : Float
     , leftLabel : String
     , rightLabel : String
     , leftValueRaw : Int
     , rightValueRaw : Int
-    , offset : Float
+    , left : Pos
+    , right : Pos
     }
 
 
@@ -51,20 +54,22 @@ calcDimensions vc ed aAddress bAddress =
         y =
             (aPos.y + bPos.y) / 2
 
-        { leftRelation, rightRelation, aOffset } =
+        { leftRelation, rightRelation, left, right } =
             if aPos.x < bPos.x then
                 { leftRelation = ed.b2a
                 , rightRelation = ed.a2b
-                , aOffset = rad
+                , left = aPos
+                , right = bPos
                 }
 
             else
                 { leftRelation = ed.a2b
                 , rightRelation = ed.b2a
-                , aOffset = -rad
+                , left = bPos
+                , right = aPos
                 }
 
-        leftValue =
+        leftLabel =
             relationToValue leftRelation
 
         rawValue =
@@ -88,35 +93,37 @@ calcDimensions vc ed aAddress bAddress =
                     )
                 >> Maybe.withDefault ""
 
-        rightValue =
+        rightLabel =
             relationToValue rightRelation
 
         charWidth =
             7.5
 
-        labelWidthLeft =
-            (String.length leftValue |> toFloat)
+        leftLabelWidth =
+            (String.length leftLabel |> toFloat)
                 * charWidth
 
-        labelWidthRight =
-            (String.length rightValue |> toFloat)
+        rightLabelWidth =
+            (String.length rightLabel |> toFloat)
                 * charWidth
 
-        fd =
-            GraphComponents.addressNodeNodeFrame_details
-
-        rad =
-            fd.width / 2
+        totalWidth =
+            Theme.aggregatedLabel_details.width
+                - Theme.aggregatedLabelRectangleOfAggregatedLabel_details.width
+                + leftLabelWidth
+                + rightLabelWidth
     in
     { x = x * unit
     , y = y * unit
-    , leftLabelWidth = labelWidthLeft
-    , rightLabelWidth = labelWidthRight
-    , leftLabel = leftValue
-    , rightLabel = rightValue
+    , totalWidth = totalWidth
+    , leftLabelWidth = leftLabelWidth
+    , rightLabelWidth = rightLabelWidth
+    , leftLabel = leftLabel
+    , rightLabel = rightLabel
     , leftValueRaw = leftValueRaw
     , rightValueRaw = rightValueRaw
-    , offset = aOffset
+    , left = left
+    , right = right
     }
 
 
@@ -129,7 +136,7 @@ view vc ed aAddress bAddress =
         halfOriginalWidth =
             originalWidth / 2
 
-        { leftLabelWidth, rightLabelWidth, x, y, leftLabel, rightLabel, leftValueRaw, rightValueRaw } =
+        { leftLabelWidth, rightLabelWidth, x, y, leftLabel, rightLabel, leftValueRaw, rightValueRaw, totalWidth } =
             calcDimensions vc ed aAddress bAddress
 
         rectangleWidth =
@@ -147,7 +154,7 @@ view vc ed aAddress bAddress =
             (Theme.aggregatedLabelAttributes
                 |> s_root
                     [ translate
-                        (x - rectangleWidth / 2)
+                        (x - totalWidth / 2)
                         (y - (Theme.aggregatedLabel_details.height / 2))
                         |> transform
                     , AggEdge.initId ed.a ed.b
@@ -226,8 +233,8 @@ view vc ed aAddress bAddress =
                     )
             )
             { root =
-                { leftValue = leftLabel
-                , rightValue = rightLabel
+                { leftValue = leftLabel |> Debug.log "leftLabel"
+                , rightValue = rightLabel |> Debug.log "rightLabel"
                 , showHighlight = False
                 }
             }
@@ -237,30 +244,94 @@ view vc ed aAddress bAddress =
 edge : View.Config -> AggEdge -> Address -> Address -> Svg Msg
 edge vc ed aAddress bAddress =
     let
-        aPos =
-            aAddress |> toPosition
-
-        bPos =
-            bAddress |> toPosition
-
-        { offset } =
+        { left, right, totalWidth, x, y } =
             calcDimensions vc ed aAddress bAddress
+                |> Debug.log "dimensions"
 
         fd =
             GraphComponents.addressNodeNodeFrame_details
+
+        rad =
+            fd.width / 2 + fd.strokeWidth
+
+        ax_ =
+            left.x * unit + rad
+
+        ax =
+            if ax_ > lx then
+                ax_ - 2 * rad
+
+            else
+                ax_
+
+        ay =
+            left.y * unit
+
+        bx_ =
+            right.x * unit - rad
+
+        bx =
+            if bx_ < rx then
+                bx_ + 2 * rad
+
+            else
+                bx_
+
+        by =
+            right.y * unit
+
+        lx =
+            x - totalWidth / 2
+
+        rx =
+            x + totalWidth / 2
+
+        maxDiff =
+            5
+
+        diffl =
+            ax
+                - lx
+                |> max 0
+
+        difflCap =
+            diffl
+                |> min maxDiff
+
+        diffr =
+            bx
+                - rx
+                |> min 0
+
+        diffrCap =
+            diffr
+                |> max (negate maxDiff)
     in
-    g
-        []
-        [ line
-            [ Svg.x1 <| String.fromFloat <| aPos.x * unit + offset
-            , Svg.y1 <| String.fromFloat <| aPos.y * unit
-            , Svg.x2 <| String.fromFloat <| bPos.x * unit - offset
-            , Svg.y2 <| String.fromFloat <| bPos.y * unit
-            , css Theme.aggregatedLinkMainLine_details.styles
-            , css
-                [ Css.property "stroke-width" <| String.fromFloat Theme.aggregatedLinkMainLine_details.strokeWidth
-                , Css.property "stroke" "black"
+    path
+        [ Svg.d <|
+            pathD
+                [ M ( ax, ay )
+                , C ( ax + (lx - ax) / 3 - diffl, y ) ( lx - difflCap, y ) ( lx, y )
+                , L ( rx, y )
+                , C ( rx - diffrCap, y ) ( rx + (bx - rx) / 3 * 2 - diffr, y ) ( bx, by )
                 ]
+        , css Theme.aggregatedLinkMainLine_details.styles
+        , css
+            [ Css.property "stroke-width" <| String.fromFloat Theme.aggregatedLinkMainLine_details.strokeWidth
+            , Css.property "stroke" "black"
+            , Css.property "fill" "transparent" |> Css.important
             ]
-            []
         ]
+        []
+
+
+
+{-
+   line
+   [ Svg.x1 <| String.fromFloat <| aPos.x * unit + offset
+   , Svg.y1 <| String.fromFloat <| aPos.y * unit
+   , Svg.x2 <| String.fromFloat <| bPos.x * unit - offset
+   , Svg.y2 <| String.fromFloat <| bPos.y * unit
+   ]
+   []
+-}
